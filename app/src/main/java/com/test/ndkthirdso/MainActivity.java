91037d2f1407090b2fa0d4ecbbbe3618b4d64eb6
tanmingfeng <tanmingfeng002@163.com>
2017-07-23 19:17:57 +0800

    添加引入自己的so引入第三方库

--- app/src/main/java/com/test/ndkthirdso/MainActivity.java
+++ app/src/main/java/com/test/ndkthirdso/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        tv.setText(stringFromJNI()+JniUtil.add(1,2));
     }
 
     /**
